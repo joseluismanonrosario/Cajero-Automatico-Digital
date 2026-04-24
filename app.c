26961525a44eee8545623929a7df40eb4913d2d1
Jose Luis Manon <joseluismanonrosario@gmail.com>
2026-04-24 10:31:46 -0400

    Base de mi programa

--- app.c
+++ app.c
@@ -2,20 +2,6 @@
 #include <string.h>
 
 int main() {
-    int a = 5;
-    int b = 10; 
-    int sum = a + b;
-
-
-    printf("Ingrese su contraseña: ");
-    char password[20];
-    scanf("%s", password);
-    if (strcmp(password, "123456") == 0) {
-        printf("The sum of %d and %d is %d\n", a, b, sum);
-        printf("Creado por: Jose Luis Manon");
-    } else {
-        printf("Contraseña incorrecta. Acceso denegado.\n");
-    }
-    
+    printf("Hello, World!\n");
     return 0;
 }
